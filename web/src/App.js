60bcb396649aaf8b1e6b6e11e10b69918a4f64e7
EverytimeAomsin <sarin542@hotmail.com>
2022-01-09 05:09:08 +0700

    แก้ css

--- web/src/App.js
+++ web/src/App.js
@@ -1,65 +1,80 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { MDBContainer, MDBInput, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBInput, MDBRow, MDBCol, MDBTypography } from 'mdb-react-ui-kit';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   const [trips, setMenu] = useState([]);
   const [photo, setphoto] = useState([]);
   useEffect(() => {
-    document.title = "เที่ยวที่ไหนดี";
+    document.title = "เที่ยวไหนดี";
     loadMenus();
   }, []);
 
   const loadMenus = async () => {
-    const result = await axios.get("http://localhost:9000/trips");
+    const result = await axios.get("http://localhost:9000/trips" + "?q="+ "คาเฟ่"  );
     setMenu(result.data.reverse());
     setphoto(result.data.reverse());
   };
+ 
+  let { id } = useParams();
+  console.log(id)
   return (
-    <div style={{ marginTop: '60px' }} className="">
+    <div style={{ marginTop: '80px' }} className="">
       <MDBRow>
-        <MDBCol md='3' className='col-example'>
+        <MDBCol md='2' className=''>
         </MDBCol>
-        <MDBCol md='6' className='col-example'>
-          <h1 style={{ fontSize: '400%' }} className='fw-bolder text-center text-info header'>เทียวที่ไหนดี</h1>
-          <div style={{ marginTop: '40px' }}>
+        <MDBCol md='8' className=''>
+          <h1 style={{ fontSize: '400%' }} className='fw-bolder text-center text-info header'>เที่ยวไหนดี</h1>
+          <div style={{ marginTop: '80px' }}>
             <MDBInput label='หาที่เที่ยวกัน' id='form1' type='text' />
+            
           </div>
           <div>
-            {trips.map((trip) => (
-              <div style={{ marginTop: '40px' }}>
-                <MDBRow>
-                  <MDBCol md='4' className='col-example'>
-                    <img src='https://cdn.discordapp.com/attachments/556546837247426635/929058679360221254/Illustration.jpg' className='img-fluid rounded' style={{ height: '100%' }}></img>
-                  </MDBCol>
-                  <MDBCol md='8' className='col-example'>
-                    <h4 className='fw-bolder Kanit700'><a className="link-dark ms-2" href={trip.url}>{trip.title}</a></h4>
-                    <p className="line-clamp-description Kanit300">{trip.description}</p>
-                    <p className="Kanit300">ลงเครื่องที่ สนามบินเถาหยวน เวลา... <a href={trip.url} className="text-decoration-underline">อ่านต่อ</a><p style={{ fontSize: '13px' }}>หมวด - กรุ๊ปกิน ทริปต่างประเทศ และ ทริปเอเชีย</p></p>
-                    <div >
-                      <div className="d-flex flex-row  mb-3">
-                        <div className="p-2"><img src='https://img.wongnai.com/p/1600x0/2019/07/02/941dbb607f0742bbadd63bbbd993e187.jpg' className='img-fluid  rounded' alt='...' /></div>
-                        <div className="p-2"><img src='https://img.wongnai.com/p/1600x0/2019/07/02/941dbb607f0742bbadd63bbbd993e187.jpg' className='img-fluid  rounded' alt='...' /></div>
-                        <div className="p-2"><img src='https://img.wongnai.com/p/1600x0/2019/07/02/941dbb607f0742bbadd63bbbd993e187.jpg' className='img-fluid  rounded' alt='...' /></div>
-                        {trips.map((photo) => (
 
-                          <div className="p-2"><img src={photo.photos} className='img-fluid  rounded' alt='...' /></div>
-                        ))}
+            {trips.map((trip) => (
+              <div style={{ marginTop: '50px' }}>
+                <MDBContainer>
+                  <MDBRow>
+                    <MDBCol md='4' className=''>
+                      <img src={trip.photos[0]} className='img-fluid rounded mhm ' ></img>
+                    </MDBCol>
+                    <MDBCol md='7' className=''>
+                      <h4 className='fw-bolder Kanit700' style={{ fontSize: '23px' }}><a className="link-dark " href={trip.url}>{trip.title}</a></h4>
+                      <MDBTypography className='lead mb-0  ' style={{ fontSize: '15px' }}>
+                        <span className="line-clamp-description">{trip.description}</span>  <a href={trip.url} className="text-decoration-underline">อ่านต่อ</a>
+                      </MDBTypography>
+                      <div className="d-flex align-items-start"><p>หมวด - 
+                      {trip.tags.map((tag) =>
+                      <Router>
+                          <Link to="คาเฟ่"><span >{tag + ","}</span></Link>
+                      
+                          </Router>
+                        )}</p></div>
+                      <div className="d-flex align-items-start">
+                        {trip.photos.map((photo) =>
+                          <div className="p-2"><img src={photo} className='img-fluid  rounded mhs' alt='...' /></div>
+                        )}
+
                       </div>
-                    </div>
-                  </MDBCol>
-                </MDBRow>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBContainer>
               </div>
             ))}
+
           </div>
-          {/* แถว 1 */}
-
-          {/* แถว 1 */}
-
 
         </MDBCol>
-        <MDBCol md='3' className='col-example'>
+        <MDBCol md='2' className='col-example'>
         </MDBCol>
       </MDBRow>
 
@@ -69,4 +84,8 @@ function App() {
   );
 }
 
+
+
 export default App;
+
+
---
+++ web/src/search.js
@@ -0,0 +1,19 @@
+import React, { Component }  from 'react';
+const SearchBar = ({ searchQuery, setSearchQuery }) => (
+    <form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search blog posts</span>
+        </label>
+        <input
+            value={searchQuery}
+            onInput={e => setSearchQuery(e.target.value)}
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name=""
+        />
+        <button type="submit">Search</button>
+    </form>
+);
+
+export default SearchBar;
